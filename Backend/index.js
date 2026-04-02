464448ba5ba4a7aa4cbf96e0dbe14f969ae5fd7d
Sanyukta1304 <dassanyukta9@gmail.com>
2026-04-02 18:51:26 +0530

    Backend setup: server + MongoDB connected

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./DataBase/connection");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
